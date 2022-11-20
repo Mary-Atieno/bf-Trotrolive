13233dfd5b6d5c7519752d6e5d33f27c5ea73d97
mary atieno <mercymary 948@gmail.com>
2022-11-20 13:50:06 +0300

    adds urls

---
+++ accounts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path('',views.base, name='base'),
+    path('',views.home, name='index'),
+    path('home',views.home, name='home'),
+    path('about',views.about, name='about'),
+    path('contact',views.contact, name='contact'),
+
+]
